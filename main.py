62649797ab90f48f87a9528efe90431a427f773a
natewise <32283998+natewise@users.noreply.github.com>
2022-07-06 11:17:24 -0700

    Switch to manual setting of sunrise and sunset
    7am and 7pm

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
 import RPi.GPIO as GPIO
-from suntime import Sun
 from datetime import datetime, timedelta
 import time
 import pytz
 
 pst=pytz.timezone('US/Pacific')
-utc=pytz.UTC
-
-# For Postal Code 92083
-latitude = 33.201374
-longitude = -117.252914
-sun = Sun(latitude, longitude)
+sunrise = datetime.now(pst).replace(hour=7, minute=0, second=0) # 7am
+sunset = sunrise + timedelta(hours=12) # 7pm 
 
 # GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-'''
-# Test GPIO
-high = False
 while True:
-    print("HIGH" if high else "LOW")
-    GPIO.output(12, high)
-    high = ~high
-    time.sleep(10)
-'''
-while True:
-    sunrise = sun.get_sunrise_time().astimezone(pst)
-    sunset = sun.get_sunset_time().astimezone(pst)
-    if sunrise > sunset: # Bug in API, time is behind
-        sunset = sunset + timedelta(days=1)
     now = datetime.now(pst) # Needed for "TypeError: can't compare offset-naive and offset-aware datetimes"
     print("Sunrise:", sunrise.strftime("%m %d %Y %H:%M:%S %Z"),
           "Sunset:", sunset.strftime("%m %d %Y %H:%M:%S %Z"),
